9b48a330d09eabf4e3d735a2f02ffa3ad789518e
Satya <satya70909@example.com>
2025-04-09 14:51:28 +0530

    config

--- obj/Debug/net8.0/WeatherBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4480fc6a64cbb27d5c2223dea95c460cee83876")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
